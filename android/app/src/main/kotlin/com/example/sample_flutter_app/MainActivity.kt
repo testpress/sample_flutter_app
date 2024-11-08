f28c64ac252eca845ef401b9fb4b97441831b245
Hari <hari@testpress.in>
2024-11-08 12:11:43 +0530

    fix: Migrate android code to support flutter latest versions

---
+++ android/app/src/main/kotlin/com/example/sample_flutter_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.tpstreams.example.sample_flutter_app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity(){
+    
+}
--- android/app/src/main/kotlin/com/tpstreams/example/sample_flutter_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tpstreams.example.sample_flutter_app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
